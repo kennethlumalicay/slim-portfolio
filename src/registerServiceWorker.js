d7d521f489595d12aa9eb29feb1b2dfe9251a177
kennethlumalicay <kennethlumalicay@gmail.com>
2018-02-25 01:06:37 -0500

    sw test

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -8,8 +8,6 @@
 // To learn more about the benefits of this model, read https://goo.gl/KwvDNy.
 // This link also includes instructions on opting out of this behavior.
 
-const REPO_PATH = '/slim-portfolio/';
-
 const isLocalhost = Boolean(
   window.location.hostname === 'localhost' ||
     // [::1] is the IPv6 localhost address.
@@ -48,7 +46,7 @@ export default function register() {
 
 function registerValidSW(swUrl) {
   navigator.serviceWorker
-    .register(swUrl, { scope: REPO_PATH })
+    .register(swUrl, { scope: '/slim-portfolio/' })
     .then(registration => {
       registration.onupdatefound = () => {
         const installingWorker = registration.installing;
