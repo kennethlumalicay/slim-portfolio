088908913cf545ceb3bbe32a52ebb95906fa11b7
kennethlumalicay <kennethlumalicay@gmail.com>
2018-03-02 03:22:18 -0500

    minor fixes

--- src/ProjectItem.js
+++ src/ProjectItem.js
@@ -1,21 +1,31 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 
 class ProjectItem extends Component {
 
   render() {
-    const { link, github, img, title } = this.props;
+    const { link, github, img, title, description, category } = this.props;
 
     return (
       <div className='project-item-container'>
         <div className='project-details'>
           <div>
             <h3 className='title'>{title}</h3>
+            {
+              category === 'featured'
+               ? <p className='description'>{ description }</p>
+               : null
+            }
           </div>
           <div>
             <a href={link} target='_blank' rel='noopener noreferrer'>visit</a>
             {
               github
-                ? <a href={github} target='_blank' rel='noopener noreferrer'>github</a>
+                ? (
+                  <Fragment>
+                    <span>|</span>
+                    <a href={github} target='_blank' rel='noopener noreferrer'>github</a>
+                  </Fragment>
+                )
                 : null
             }
           </div>
