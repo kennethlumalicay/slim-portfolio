232ac8acd96f3f1e97ab3c16755a5d7ca68bd8c6
kennethlumalicay <kennethlumalicay@gmail.com>
2018-03-02 13:04:38 -0500

    test success

--- src/ProjectItem.js
+++ src/ProjectItem.js
@@ -1,37 +1,17 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component } from 'react';
 
 class ProjectItem extends Component {
 
   render() {
-    const { link, github, img, title, description, category } = this.props;
+    const { img, selected } = this.props;
 
     return (
-      <div className='project-item-container'>
-        <div className='project-details'>
-          <div>
-            <h3 className='title'>{title}</h3>
-            {
-              category === 'featured'
-               ? <p className='description'>{ description }</p>
-               : null
-            }
-          </div>
-          <div>
-            <a href={link} target='_blank' rel='noopener noreferrer'>website</a>
-            {
-              github
-                ? (
-                  <Fragment>
-                    <span>|</span>
-                    <a href={github} target='_blank' rel='noopener noreferrer'>github</a>
-                  </Fragment>
-                )
-                : null
-            }
-          </div>
-        </div>
+      <div
+        className={'project-item-container' + (selected ? ' selected' : '')}
+        onClick={this.props.select}
+      >
         <div
-          className={'project-item'}
+          className='project-item'
           style={{backgroundImage: `url('./img/${img}')`}}
         >
         </div>
--- src/Projects.js
+++ src/Projects.js
@@ -1,9 +1,20 @@
 import React, { Component } from 'react';
+import { CSSTransition } from 'react-transition-group';
 import './Projects.css';
 import projectsJson from './projects.json';
 import ProjectItem from './ProjectItem.js';
+import ProjectShown from './ProjectShown.js';
 
 class Projects extends Component {
+  constructor(props) {
+    super(props);
+    this.interval = null;
+    this.state = {
+      selected: 0,
+      pending: 0,
+      changeSelect: true
+    }
+  }
 
   categorizeProjects(items) {
     let categorized = {};
@@ -38,14 +49,82 @@ class Projects extends Component {
     ));
   }
 
-  render() {
-    const categorized = this.categorizeProjects(projectsJson.items);
-    const renderedProjects = this.renderCategories(categorized);
+  handleMove(right) {
+    const { listElem } = this.refs;
+    const { scrollWidth, clientWidth } = listElem;
+    const move = clientWidth/2;
+    const maxWidth = scrollWidth - clientWidth;
+    const divisor = clientWidth < 768 ? 50 : 80;
+    clearInterval(this.interval);
+    this.interval = setInterval(() => {
+      if((right && listElem.scrollLeft >= maxWidth)
+        || (!right && listElem.scrollLeft <= 0)
+      ) {
+        clearInterval(this.interval);
+      }
+      listElem.scrollLeft += right ? move/divisor : -move/divisor;
+    });
+  }
 
+  handleMoveStop() {
+    clearInterval(this.interval);
+  }
+
+  handleSelect(index) {
+    const { selected } = this.state;
+    this.setState({
+      pending: index,
+      changeSelect: selected === index
+    });
+  }
+
+  handleExited() {
+    const { pending } = this.state;
+    this.setState({
+      selected: pending,
+      changeSelect: true
+    });
+  }
+
+  render() {
+    const { pending, changeSelect } = this.state;
+    const projectsList = projectsJson.items.map((e,i) => (
+      <ProjectItem
+        key={e.title.replace(/\W/g, '')}
+        {...e}
+        select={event => this.handleSelect(i)}
+        selected={pending===i}
+      />
+    ));
+    const shownItem = projectsJson.items[this.state.selected];
     return (
       <section className='projects'>
         <h1>Projects</h1>
-          { renderedProjects }
+        <CSSTransition
+          in={changeSelect}
+          classNames='project'
+          timeout={350}
+          onExited={() => this.handleExited()}
+        >
+          <ProjectShown {...shownItem}/>
+        </CSSTransition>
+        <div className='projects-container'>
+          <button
+            onMouseDown={event => this.handleMove(false)}
+            onMouseUp={event => this.handleMoveStop()}
+            onTouchStart={event => this.handleMove(false)}
+            onTouchEnd={event => this.handleMoveStop()}
+          ><img src='/img/arrow-left.png' alt='<'/></button>
+          <div className='projects-list' ref='listElem'>
+            { projectsList }
+          </div>
+          <button
+            onMouseDown={event => this.handleMove(true)}
+            onMouseUp={event => this.handleMoveStop()}
+            onTouchStart={event => this.handleMove(true)}
+            onTouchEnd={event => this.handleMoveStop()}
+          ><img src='/img/arrow-right.png' alt='>'/></button>
+        </div>
       </section>
     );
   }
